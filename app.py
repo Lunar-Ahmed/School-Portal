b9c83a599076d650c168d62d476825f1b9161130
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-06-03 00:26:55 +0100

    updates

--- app.py
+++ app.py
@@ -220,9 +220,9 @@ def user_login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
-        modified_username = 'shs' + username
         password = request.form['password']
-        modified_username = 'pry' + username
+        primary = 'pry'
+        modified_username = primary + username
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM reg WHERE username = % s AND password = % s', (modified_username, password, ))
         account = cursor.fetchone()
